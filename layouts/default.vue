9b33820caf6c0b3c08dd8ba80c1dbb1613d602c2
Lyall <lyall@iriesoft.net>
2023-07-17 20:17:38 +0200

    dynamic layouts

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p class="font-normal">
+      Some default HEADER layout shared across all pages
+    </p>
+    <slot />
+
+    <p class="font-normal">
+      Some default FOOTER layout shared across all pages
+    </p>
+  </div>
+</template>
